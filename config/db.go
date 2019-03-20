9ad501c17afeb8a88ffc4b1349f3518167af7861
rburawes <burawes.rae@gmail.com>
2019-03-20 22:47:06 +0800

    moved connection properties to a file

--- config/db.go
+++ config/db.go
@@ -2,7 +2,10 @@ package config
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+
 	// Needed only for initialization.
 	_ "github.com/lib/pq"
 )
@@ -11,15 +14,59 @@ import (
 // database tables and executing queries.
 var Database *sql.DB
 
+// Config holds the connection properties for the database.
+type Config struct {
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Database   string `json:"database"`
+	SslMode    string `json:"sslMode"`
+	URL        string `json:"url"`
+	DriverName string `json:"driverName"`
+}
+
+// Initializes database connection.
 func init() {
 	var err error
-	Database, err = sql.Open("postgres", "postgres://postgres:postgres@localhost/currency?sslmode=disable")
+
+	config, err := loadConnectionProperties()
+
 	if err != nil {
 		panic(err)
+		fmt.Println("Invalid connection details: " + err.Error())
+	}
+
+	Database, err = sql.Open(config.DriverName, config.GetConnectionString())
+	if err != nil {
+		panic(err)
+		fmt.Println("Unable to connect to the database: " + err.Error())
 	}
 
 	if err = Database.Ping(); err != nil {
 		panic(err)
 	}
 	fmt.Println("Database connection successful.")
+}
+
+// Parses the config file and load to Config struct.
+func loadConnectionProperties() (Config, error) {
+
+	var config Config
+	data, err := ioutil.ReadFile("./config.json")
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+
+}
+
+// GetConnectionString returns the data source value or connection string.
+func (config Config) GetConnectionString() string {
+	return fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s", config.Username, config.Password, config.URL, config.Database, config.SslMode)
 }
